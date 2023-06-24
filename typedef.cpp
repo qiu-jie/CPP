9177ba95204f21b39c3e13235eeeeddd153f9bb1
qiu_jie <gugoo@outlook.com>
2023-06-25 00:11:44 +0800

    typedef

---
+++ typedef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+typedef int myint;
+
+typedef unsigned char vec3b[3];
+
+typedef struct _rgb_struct{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} rgb_struct;
+
+myint num=32;
+unsigned char color[3];
+vec3b color ={255,0,255};
+
+rgb struct rgb={0,255,128};
