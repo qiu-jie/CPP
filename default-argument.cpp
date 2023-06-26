eb8fcda00f1627dbf65ac9dd39814b912ddf7b3b
qiu_jie <gugoo@outlook.com>
2023-06-26 13:36:42 +0800

    参数调用

---
+++ default-argument.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//默认参数应该从尾部开始定义
+float norm(float x,float y,float z);
+float norm(float x,float y,float z=0);
+float norm(float x,float y=0,float z);
+//ERROR，重复定义，只能设一次
+//float norm(float x,float y=0,float z=4);
+
+
+int main()
+{
+    cout<<norm(3.0f)<<endl;
+    cout<<norm(3.0f,4.0f)<<endl;
+    cout<<norm(3.0f,4.0f,5.0f)<<endl;
+    return 0;
+}
+
+float norm(float x.float y,float z)
+{
+    return sqrt(x*x+y*y+z*z);
+}
